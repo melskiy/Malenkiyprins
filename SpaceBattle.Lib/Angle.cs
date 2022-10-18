f091830307b9c742594c3b6c63ecdc0d4853c8d5
melskiy <87464481+melskiy@users.noreply.github.com>
2022-10-18 14:17:27 +0600

    &

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -1,6 +1,10 @@
 namespace SpaceBattle.Lib;
 public class Angle
 {
+    public static int SCD(int a, int b)
+    {
+        return b == 0 ? a : SCD(b, a % b);
+    }
     private int numerator;
     private int denominator;
     public Angle(int n, int d)
@@ -19,10 +23,6 @@ public class Angle
 
     public static Angle operator +(Angle a1, Angle a2)
     {
-        int SCD(int a, int b)
-        {
-            return b == 0 ? a : SCD(b, a % b);
-        }
         int y3 = SCD(a1.numerator * a2.denominator + a2.numerator * a1.denominator, a1.denominator * a2.denominator);
         return new Angle((a1.numerator * a2.denominator + a2.numerator * a1.denominator) / y3, a1.denominator * a2.denominator / y3);
     }
