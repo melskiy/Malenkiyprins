4aa076fc50f66635e0be73e2724ebc4464d3e605
melskiy <87464481+melskiy@users.noreply.github.com>
2022-11-11 01:02:29 +0600

    смээртб

--- SpaceBattle.Lib/Uobject/GenerateAdapter.cs
+++ SpaceBattle.Lib/Uobject/GenerateAdapter.cs
@@ -8,16 +8,7 @@ public class GenerateAdapter : IStrategy
     {
         Type a = (Type)args[0];
         IUObject obj = (IUObject)args[1];
-
-        if (Activator.CreateInstance(a, obj) is null)
-        {
-            throw new ArgumentNullException();
-        }
-        else
-        {
-            return Activator.CreateInstance(a, obj);
-        }
-
+        return Activator.CreateInstance(a,obj)!;
+       
     }
-
 }
