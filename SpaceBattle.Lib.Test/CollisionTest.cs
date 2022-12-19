24adf9fea9d825d3b4b70a55b725c5ea63544e25
ZotovNikita <113326727+ZotovNikita@users.noreply.github.com>
2022-12-19 11:26:08 +0600

    Update CollisionTest.cs

--- SpaceBattle.Lib.Test/CollisionTest.cs
+++ SpaceBattle.Lib.Test/CollisionTest.cs
@@ -5,7 +5,7 @@ namespace SpaceBattle.Lib.Test;
 public class CollisionTest
 {    
     [Fact]
-    public void CollisionTests()
+    public void CollisionTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
