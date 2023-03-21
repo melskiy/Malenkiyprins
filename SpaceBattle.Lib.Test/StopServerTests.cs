702009f9e51509682ab79199b73e328e8cb1e11b
melskiy <87464481+melskiy@users.noreply.github.com>
2023-03-21 14:14:00 +0600

    altshiftF_FIX2

--- SpaceBattle.Lib.Test/StopServerTests.cs
+++ SpaceBattle.Lib.Test/StopServerTests.cs
@@ -12,8 +12,7 @@ public class StopServerTests
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
-        
-        
+
         var mockSendCommandStrategy = new Mock<ICommand>();
         var mockThreadMap = new ConcurrentDictionary<int, ISender>();
         var send = new Mock<ISender>();
@@ -22,28 +21,24 @@ public class StopServerTests
         mockThreadMap[2] = send.Object;
         mockThreadMap[3] = send.Object;
 
-        
+
         var mockCommand = new Mock<SpaceBattle.Lib.ICommand>();
         int i = 0;
-        mockCommand.Setup(x => x.Execute()).Callback(() => {i+=1; });
+        mockCommand.Setup(x => x.Execute()).Callback(() => { i += 1; });
         var mockStrategyWithParams = new Mock<IStrategy>();
         mockStrategyWithParams.Setup(x => x.DoAlgorithm(It.IsAny<object[]>())).Returns(mockCommand.Object).Verifiable();
 
 
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SenderMap", (object[] args) => mockThreadMap).Execute();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "StopServerStrategy",  (object[] args) =>  new StopServerCommand()).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "StopServerStrategy", (object[] args) => new StopServerCommand()).Execute();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SendCommandStrategy", (object[] args) =>  mockStrategyWithParams.Object.DoAlgorithm(args) ).Execute();
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SoftStopServerThreadCommandStrategy", (object[] args) =>  mockStrategyWithParams.Object.DoAlgorithm(args) ).Execute();
-       
-       
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SendCommandStrategy", (object[] args) => mockStrategyWithParams.Object.DoAlgorithm(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SoftStopServerThreadCommandStrategy", (object[] args) => mockStrategyWithParams.Object.DoAlgorithm(args)).Execute();
+
+
         IoC.Resolve<ICommand>("StopServerStrategy").Execute();
 
-
-
-        Assert.Equal(3,i);
-
-
+        Assert.Equal(3, i);
     }
 }
