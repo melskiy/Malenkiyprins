027ce72ff65e65aea8923a972911dcb5ba6a2333
ZotovNikita <113326727+ZotovNikita@users.noreply.github.com>
2023-05-23 08:40:32 +0600

    Лабораторная работа 10 часть 1 (#16)
    * lab10part1
    
    * architecture fix
    
    * template fix
    
    * semantic fix

---
+++ SpaceBattle.Lib.Test/CodeStringGenerateStrategyTest.cs
@@ -0,0 +1,31 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+namespace SpaceBattle.Lib.Test;
+
+public class CodeStringGenerateStrategyTest
+{
+    [Fact]
+    public void SuccessfulCodeStringGenerateStrategy()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        var scope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+
+        var str = "rendered_template";
+
+        var adapterBuilder = new Mock<IBuilder>();
+        adapterBuilder.Setup(c => c.Build()).Returns(str).Verifiable();
+        adapterBuilder.Setup(c => c.AddMembers(It.IsAny<object>())).Callback(() => {}).Verifiable();
+
+        var getAdapterBuilderStrategy = new Mock<IStrategy>();
+        getAdapterBuilderStrategy.Setup(s => s.DoAlgorithm(It.IsAny<object[]>())).Returns(adapterBuilder.Object).Verifiable();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "GetAdapterBuilder", (object[] args) => getAdapterBuilderStrategy.Object.DoAlgorithm(args)).Execute();
+
+        var codeStringGenerateStrategy = new CodeStringGenerateStrategy();
+        var codeString = (string)codeStringGenerateStrategy.DoAlgorithm(typeof(IUObject), typeof(IMovable));
+
+        Assert.True(codeString == str);
+        adapterBuilder.VerifyAll();
+        getAdapterBuilderStrategy.VerifyAll();
+    }
+}
---
+++ SpaceBattle.Lib/Interfaces/IBuilder.cs
@@ -0,0 +1,8 @@
+namespace SpaceBattle.Lib;
+
+public interface IBuilder
+{
+
+    public string Build();
+    public void AddMembers(object member);
+}
